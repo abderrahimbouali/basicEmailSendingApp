0499efdd418ddb6bd19a069eda245bda714673b3
abderrahimbouali <33182559+abderrahimbouali@users.noreply.github.com>
2023-01-14 14:46:54 +0100

    basic example to be improved later

---
+++ index2.php
@@ -0,0 +1,41 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/phpmailer/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer();
+
+$from = "testtest1312testtest@gmail.com";
+$password = "eukmkzqkzzrybtjb";
+$to = $_POST["email"];
+$body = $_POST["body"];
+
+try {
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->SMTPAuth = true;
+
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    $mail->Username = $from;
+    $mail->Password = $password;
+
+    $mail->setFrom($from, 'Test Mailer');
+    $mail->addAddress($to, 'Test Receiver');
+
+    $mail->Subject = 'SMTP Test Subject';
+    $mail->Body    = $body;
+
+    $mail->send();
+    echo 'Message has been sent \_(0 _0)_/';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
